0e8a8079cd7d2944c93caf6893cb8a1aceb8ac9b
Carlos Salamanca <salamancacmcol@gmail.com>
2024-07-17 18:34:59 -0500

    Update

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,4 +1,11 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Invoices',
+};
+
+export const experimental_ppr = true;
  
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
